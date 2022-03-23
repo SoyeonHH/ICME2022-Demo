9c5222a3278e7b648589d4504369c7f41de907b5
LiiNen <kjeonghoon065@gmail.com>
2022-03-23 20:44:01 +0900

    Init html page

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+var http = require('http');
+var fs = require('fs');
+
+const port = 3000;
+
+app.use(cors());
+app.use('/script', express.static(__dirname + '/script'));
+app.use('/style', express.static(__dirname + '/style'));
+app.use('/data', express.static(__dirname + '/data'));
+
+var server = http.createServer(app).listen(port, function() {
+  console.log('Server run');
+})
+
+app.get('/', function(req, res) {
+  fs.readFile('index.html', function(error, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  });
+});
---
+++ script/listener.js
@@ -0,0 +1,44 @@
+
+var test = Array.from(Array(30).keys())
+var formatList = ['mp4', 'wav', 'txt']
+
+selectList = document.querySelectorAll('.selectWrapper');
+
+var clickName = '';
+
+test.map((e) => {
+  item = document.createElement('div');
+  item.className = `selectItem item${e}`;
+  item.innerText = 'testtesttesttest';
+  itemC1 = item.cloneNode(true);
+  itemC2 = item.cloneNode(true);
+  item.innerText = 'testtest' + e + '.' + formatList[0]
+  itemC1.innerText = 'testtesttest' + e + '.' + formatList[1]
+  itemC2.innerText = 'testtesttesttest' + e + '.' + formatList[2]
+  let name = e;
+  item.addEventListener('click', function() {
+    clickListener(name);
+  });
+  itemC1.addEventListener('click', function() {
+    clickListener(name);
+  });
+  itemC2.addEventListener('click', function() {
+    clickListener(name);
+  });
+  itemList = [item, itemC1, itemC2];
+  for(var i=0; selectList[i]; i++) {
+    selectList[i].appendChild(itemList[i]);
+  }
+});
+
+function clickListener(name) {
+  console.log(name);
+  let prev = document.querySelectorAll('.selectItem.clicked');
+  for(var i=0; prev[i]; i++) {
+    prev[i].classList.remove('clicked');
+  }
+  let next = document.querySelectorAll(`.selectItem.item${name}`);
+  for(var i=0; next[i]; i++) {
+    next[i].classList.add('clicked');
+  }
+}
