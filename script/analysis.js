ffb89abb6c64fe69161095b7dbb02610d3539ec4
oss0430 <82088950+oss0430@users.noreply.github.com>
2022-03-24 14:56:38 +0900

    at Class Analysis, added updateGraph Method
    added updateGraph method to Analysis class.
    
    added progress bar example to analysis page

--- script/analysis.js
+++ script/analysis.js
@@ -1,8 +1,33 @@
 import Chart from './chart.js-3.71/chart.js/auto'
 
-var context=document
-  .getElementByld('graph_class_2')
-  .getContext('2d');
+var graph_2 = document.getElementById('#graph_class_2');
+var graph_7 = document.getElementById('#graph_class_7');
+
+const labels2class = [
+  'Negative',
+  'Positive'
+]
+const labels7class = [
+  "Very Negative",
+  "Negative",
+  "Slightly Negative",
+  "Neutral",
+  "Slightly Positive",
+  "Positive",
+  "Very Positive",
+]
+
+const config = {
+  type: 'bar',
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        suggestedMax:100
+      }
+    }
+  },
+};
 
 function Model(classType, modelName, publisher, result) {
   this.classType = classType //2 or 7 class
@@ -18,33 +43,150 @@ function Model(classType, modelName, publisher, result) {
 
     //process input file and make this.result
   }
+
+  this.getClassAcc = function() {//return class accuracy from model's result file
+    var returnAcc2 = [0,0]
+    var returnAcc7 = [0,0,0,0,0,0,0]
+
+    if(this.result) {//result exist.
+      //2 class result
+      var cnt2class = [0,0]
+      var hit2class = [0,0]
+
+      for (var i = 0; i < this.result.length; i++) {
+        var j = -1
+        switch(result[i][5]) {
+          case 'negative'         : j = 0; break;
+          case 'positive'         : j = 1; break;
+          default : break; //err uknown class
+        }
+
+        cnt2class[j] = cnt2class[j] + 1
+        if(result[i][5] == result[i][8]) {//hit
+          hit2class[j] = hit2class[j] + 1
+        }
+      }
+
+      for (var i = 0; i <2; i++) {
+        returnAcc2[i] = hit2class[i] / cnt2class[i] * 100
+      }
+
+
+      var cnt7class = [0,0,0,0,0,0,0]
+      var hit7class = [0,0,0,0,0,0,0]
+
+      for (var i = 0; i < this.result.length; i++) {
+        var j = -1
+        switch(result[i][6]) {
+          case 'very negative'    : j = 0; break;
+          case 'negative'         : j = 1; break;
+          case 'slightly negative': j = 2; break;
+          case 'neutral'          : j = 3; break;
+          case 'slightly positive': j = 4; break;
+          case 'positive'         : j = 5; break;
+          case 'very positive'    : j = 6; break;
+          default : break; //err uknown class
+        }
+
+        cnt7class[j] = cnt7class[j] + 1
+        if(result[i][6] == result[i][9]) {
+          hit7class[j] = hit7class[j] + 1
+        }
+      }
+
+      for (var i = 0; i <7; i++) {
+        returnAcc7[i] = hit7class[i] / cnt7class[i] * 100
+      }
+    }
+
+    return returnAcc2, returnAcc7
+  }
 }
 
-function Analysis(goldModel, model, classType) {
-  this.classType = classType
-  this.currentVideo = 0
+function Analysis(goldModel, model) {
+
   this.goldModel = goldModel
   this.model = model
+  this.graph2 = graph_2
+  this.graph7 = graph_7
+  this.currentVideo = 0
+  this.currentClip = 0
 
   this.updateGraph = function(){
     //create class accuracy bar-graph
-    var graph_2class = new Chart(context,{
-      type:bar,
-      data:{labels:['0','1'], datasets:[{
-        lable:'2class',
-        fill:false,
-        data:[80,60],
-        borderColor:['black','black'],
-        borderWidth:1
-      }
+    var analysisDataset2;
+    var analysisDataset7;
 
-    )
     if(this.model) {//our model is uploaded
+      var goldData = this.goldModel.getClassAcc()
+      var ourData = this.model.getClassAcc()
 
-    } else {
+      analysisDataset2 = {
+        labels: labels2class,
+        datasets: [{
+          label: 'Gold',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: goldData[0],
+        },
+        {
+          label: 'Our',
+          backgroundColor: 'rgb(122, 99, 132)',
+          borderColor: 'rgb(122, 99, 132)',
+          data: ourData[0],
+        }]
+      };
 
+      analysisDataset7 = {
+        labels: labels7class,
+        datasets: [{
+          label: 'Gold',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: goldData[1],
+        },
+        {
+          label: 'Our',
+          backgroundColor: 'rgb(122, 99, 132)',
+          borderColor: 'rgb(122, 99, 132)',
+          data: ourData[1],
+        }]
+      };
 
+    } else {//no model has been uploaded show only gold Model results
+      var goldData = this.goldModel.getClassAcc()
+
+      analysisDataset2 = {
+        labels: labels2class,
+        datasets: [{
+          label: 'Gold',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: goldData[0],
+        }]
+      };
+
+      analysisDataset7 = {
+        labels: labels7class,
+        datasets: [{
+          label: 'Gold',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: goldData[1],
+        }]
+      };
     }
+
+    new Chart(
+      this.graph2,
+      data:analysisDataset2,
+      config
+    );
+    new Chart(
+      this.graph7,
+      data:analysisDataset7,
+      config
+    );
   }
 
   this.updatePbarVideo = function(video_id){//Draw Progress Bar of a single Video
@@ -83,6 +225,6 @@ var ourResult
 
 var goldModel = new Model(2, "GOLD", "donghoon", goldResult)
 var ourModel = new Model(2, "OUR", "ME", ourResult)
-var analysis = new Analysis(goldModel, ourModel, 2)
+var analysis = new Analysis(goldModel, ourModel)
 
 analysis.updateGraph
