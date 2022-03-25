b390cf81b6083169e49cfec7e94e6fcac3a419b3
LiiNen <kjeonghoon065@gmail.com>
2022-03-25 20:33:03 +0900

    Add csv getter

--- routes/localFileSystem.js
+++ routes/localFileSystem.js
@@ -1,5 +1,7 @@
 // return file list
 
+const { resourceLimits } = require('worker_threads');
+
 module.exports = () => {
   var router = require('express').Router();
   var fs = require('fs');
@@ -23,6 +25,29 @@ module.exports = () => {
       }
     }
     res.json(fileList);
+  });
+
+  router.get('/csv', (req, res) => {
+    var data = fs.readFileSync('./prediction/MAG-BERT-MOSI_result.csv', {encoding: 'utf8'});
+    var rows = data.split('\n');
+    var csv = [];
+
+    for(var rowIndex in rows) {
+      var row = rows[rowIndex].split(',');
+      if(rowIndex === "0") {
+        var columns = row;
+      }
+      else {
+        var data = {};
+        for(var columnIndex in columns) {
+          var column = columns[columnIndex];
+          data[column] = row[columnIndex];
+        }
+        csv.push(data);
+      }
+    }
+
+    res.json(csv);
   });
 
   return router;
